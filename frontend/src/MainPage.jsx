e233eaabc0e892993ad8c1c3bfa6c82248f2a4f5
devesh7yadav <devesh7yadav@gmail.com>
2026-07-06 22:25:33 -0400

    Added the ability to export the summary

--- frontend/src/MainPage.jsx
+++ frontend/src/MainPage.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { jsPDF } from "jspdf";
 import { LuCopy, LuCopyCheck } from "react-icons/lu";
+import { PiExport } from "react-icons/pi";
 
 
 function MainPage() {
@@ -10,9 +12,10 @@ function MainPage() {
         type: "",
         length: ""
     });
-    const [result, setResult] = useState("---");
+    const [result, setResult] = useState("Your summary will appear here.");
     const [isLoading, setIsLoading] = useState(false);
     const [isCopied, setIsCopied] = useState(false);
+    const [isExported, setIsExported] = useState(false);
 
     //Updates everything
     const handleChange = (e) => {
@@ -68,7 +71,7 @@ function MainPage() {
             type: "",
             length: ""
         });
-        setResult("---");
+        setResult("Your summary will appear here.");
         setIsLoading(false);
     }
 
@@ -80,12 +83,55 @@ function MainPage() {
         setTimeout(() => setIsCopied(false), 3000);
     }
 
+    //Exports result to a pdf
+    const handleExport = async () => {
+        const doc = new jsPDF({
+            orientation: "portrait",
+            unit: "mm",
+            format: "a4"
+        });
+
+        //Page Properties
+        const margin = 25.4;
+        const lineHeight = 8.4
+        const pageWidth = doc.internal.pageSize.getWidth();
+        const pageHeight = doc.internal.pageSize.getHeight();
+        const contentWidth = pageWidth - margin * 2;
+
+        doc.setFont("ariel", "normal");
+        doc.setFontSize(12);
+
+        //Splits it into lines
+        const lines = doc.splitTextToSize(result, contentWidth);
+
+        let y = margin;
+
+        //Starts at the top, writes a line, moves down, reaches end of page, adds a new page, restarts at top
+        for (let i = 0; i < lines.length; i++){
+            if (y > pageHeight - margin) {
+                doc.addPage();
+                y = margin;
+            }
+
+            doc.text(lines[i], margin, y);
+            y += lineHeight;
+        }
+
+        doc.save("Summary.pdf");
+
+        setIsExported(true);
+        setTimeout(() => setIsExported(false), 10000);
+    }
+
+    //Styles
+    const dropdown = "h-10 flex-1 border border-[#0E7488] text-sm md:text-base rounded-md shadow-md px-3 text-[#222222] bg-[#d1dfdc] focus:outline-none focus:ring-2 focus:ring-[#0E7488] cursor-pointer hover:text-[#2B54A1]";
+
     return (
       <div className="grid place-items-center px-16">
             
         <div className="w-full max-w-7xl grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-20 my-6">
 
-            <div className="grid max-h-256">
+            <div className="grid max-h-136">
                 <form onSubmit={handleSubmit} onReset={handleReset} className="grid grid-cols-1 h-full border border-[#0E7488] p-6 gap-y-8 rounded-xl shadow-xl">
 
                     <textarea 
@@ -105,7 +151,7 @@ function MainPage() {
                             name="type" 
                             id="type"
                             onChange={handleChange}
-                            className="h-10 flex-1 border border-[#0E7488] text-sm md:text-base rounded-md shadow-md px-3 text-[#222222] bg-[#d1dfdc] focus:outline-none focus:ring-2 focus:ring-[#0E7488] cursor-pointer hover:text-[#2B54A1]"
+                            className={dropdown}
                         >
                             <option value="">Select</option>
                             <option value="Paragraphs">Paragraph</option>
@@ -119,7 +165,7 @@ function MainPage() {
                             name="length" 
                             id="length"
                             onChange={handleChange}
-                            className="h-10 flex-1 border border-[#0E7488] text-sm md:text-base rounded-md shadow-md px-3 text-[#222222] bg-[#d1dfdc] focus:outline-none focus:ring-2 focus:ring-[#0E7488] cursor-pointer hover:text-[#2B54A1]"
+                            className={dropdown}
                         >
                             <option value="">Select</option>
                             <option value="Short">Short</option>
@@ -136,25 +182,41 @@ function MainPage() {
                 </form>
             </div>
 
-            <div className="grid grid-rows-[12fr_1fr] h-full max-h-256 border border-[#0E7488] rounded-xl shadow-xl">
+            <div className="grid grid-rows-[12fr_1fr] h-full max-h-136 border border-[#0E7488] rounded-xl overflow-hidden shadow-xl">
 
                 <div className="output-box border-b border-b-[#0E7488] overflow-y-auto p-6 rounded-sm shadow-lg text-[#222222] whitespace-pre-wrap">
                     {isLoading ? "Loading..." : result}
                 </div>
 
-                <button onClick={handleCopy} className="flex items-center justify-center gap-2 w-full bg-[#d1dfdc] rounded-xl shadow-xl cursor-pointer hover:text-[#2B54A1] hover:font-bold">
-                    {isCopied ? (
-                        <>
-                            Copied! 
-                            <LuCopyCheck color="green" />
-                        </>
-                        ) : (
-                        <>
-                            Copy 
-                            <LuCopy />
-                        </>
-                    )}
-                </button>
+                <div className="grid grid-cols-2">
+                    <button onClick={handleCopy} className="flex items-center justify-center gap-2 w-full bg-[#d1dfdc] rounded-bl-xl shadow-xl cursor-pointer hover:text-[#2B54A1] hover:font-bold">
+                        {isCopied ? (
+                            <>
+                                Copied! 
+                                <LuCopyCheck color="green" />
+                            </>
+                            ) : (
+                            <>
+                                Copy 
+                                <LuCopy />
+                            </>
+                        )}
+                    </button>
+
+                    <button onClick={handleExport} className="flex items-center justify-center gap-2 w-full bg-[#d1dfdc] rounded-br-xl border-l shadow-xl cursor-pointer hover:text-[#2B54A1] hover:font-bold">
+                        {isExported ? (
+                            <>
+                                Exported! 
+                                <PiExport color="green"/>
+                            </>
+                            ) : (
+                            <>
+                                Export 
+                                <PiExport />
+                            </>
+                        )}
+                    </button>
+                </div>
 
             </div>
 
